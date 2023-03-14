a88d4c39d8e063256adfc836673ee8a4739bf93a
DESKTOP-GNOW\gnow <gnow@chungbuk.ac.kr>
2023-03-14 15:09:45 +0900

    React State

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useState} from 'react'
 
 function Article(props) {
   return <article>
@@ -26,7 +26,7 @@ function Nav(props){
       <a id={t.id} href={'/read/'+t.id} onClick={event=>{
         event.preventDefault();
         // event.target.id를 통해 Nav의 id를 받아옴
-        props.onChangeMode(event.target.id); 
+        props.onChangeMode(Number(event.target.id)); 
       }}>{t.title}</a>
       </li>)
   }
@@ -38,20 +38,37 @@ function Nav(props){
 }
 
 function App() {
+  const [mode, setMode] = useState('WELCOME');
+  const [id, setId] = useState(null);
   const topics = [  // 정보가 여러개라 배열로 지정
     {id:1, title:'html', body:'html is ...'},
     {id:2, title:'css', body:'css is ...'},
     {id:3, title:'javascript', body:'javascript is ...'}
   ]
+  let content = null;
+  if(mode === 'WELCOME'){
+    content = <Article title="Welcome" body="Hello, WEB"></Article>
+  } else if(mode === 'READ'){
+    let title, body = null;
+    for(let i=0; i<topics.length; i++) {
+      console.log(topics[i].id, id);
+      if(topics[i].id == id){
+        title = topics[i].title;
+        body = topics[i].body;
+      }
+    }
+    content = <Article title={title} body={body}></Article>
+  }
   return (
     <div>
       <Header title="WEB" onChangeMode={()=>{
-        alert('Header');
+        setMode('WELCOME');
       }}></Header>
-      <Nav topics={topics} onChangeMode={(id)=>{
-        alert(id);  // id를 출력
+      <Nav topics={topics} onChangeMode={(_id)=>{
+        setMode('READ')
+        setId(_id);
       }}></Nav>
-      <Article title="welcome" body="Hello, WEB"></Article>
+      {content}
     </div>
   );
 }
